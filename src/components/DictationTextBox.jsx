bfc715f01f97937a1d8d413041ef1dd759e8378d
AnDT1211 <anthaido.bk@gmail.com>
2024-02-18 01:44:45 +0700

    hidden fix

--- src/components/DictationTextBox.jsx
+++ src/components/DictationTextBox.jsx
@@ -3,23 +3,25 @@ import TextField from "@mui/material/TextField";
 
 export default function DictationTextBox(props) {
   return <>
-    <Stack direction="row" spacing={2} className="my-5" hidden={props.hide}>
-      {/*{showTranscript()}*/}
-      <div className="container mx-auto my-5">
-        <TextField
-          autoFocus
-          id="outlined-multiline-flexible"
-          label="Type what you are listen here."
-          multiline
-          minRows={20}
-          fullWidth
-          // value={props.content}
-          value={props.content}
-          onChange={(e) => props.setContent(e.target.value)}
-          // hidden={hide}
-        />
-      </div>
-    </Stack>
+    <div hidden={props.hide}>
+      <Stack direction="row" spacing={2} className="my-5" >
+        {/*{showTranscript()}*/}
+        <div className="container mx-auto my-5">
+          <TextField
+            autoFocus
+            id="outlined-multiline-flexible"
+            label="Type what you are listen here."
+            multiline
+            minRows={20}
+            fullWidth
+            // value={props.content}
+            value={props.content}
+            onChange={(e) => props.setContent(e.target.value)}
+            // hidden={hide}
+          />
+        </div>
+      </Stack>
+    </div>
   </>
 }
 
